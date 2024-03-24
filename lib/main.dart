97878c1d99783c94239d2f3c937dabf5a5a2ae25
LbnMahs <labanmahs2002@gmail.com>
2024-03-24 21:42:53 +0300

    update: firebase setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(const MyApp());
+import 'package:vulcane/firebase_options.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform
+  );
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
