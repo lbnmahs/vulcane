8194c6647937af4f6107594ac040d0b45af407fa
LbnMahs <labanmahs2002@gmail.com>
2024-03-25 16:48:38 +0300

    chore: authentication events

--- lib/middleware/auth_bloc/auth_event.dart
+++ lib/middleware/auth_bloc/auth_event.dart
@@ -2,3 +2,26 @@ part of 'auth_bloc.dart';
 
 @immutable
 sealed class AuthEvent {}
+
+class LoginWithEmailPassword extends AuthEvent {
+  final String email;
+  final String password;
+
+  LoginWithEmailPassword({required this.email, required this.password});
+}
+
+class RegisterWithEmailPassword extends AuthEvent {
+  final String email;
+  final String password;
+  final String fullName;
+
+  RegisterWithEmailPassword({
+    required this.email, required this.password, required this.fullName
+  });
+}
+
+class SignOut extends AuthEvent {}
+
+class SignInWithGoogle extends AuthEvent {}
+
+class CheckAuthEvent extends AuthEvent {}
