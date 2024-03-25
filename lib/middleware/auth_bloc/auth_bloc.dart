b82f0fe7d8aa2935766aa7c928af36585db0c862
LbnMahs <labanmahs2002@gmail.com>
2024-03-25 16:44:24 +0300

    chore: bloc authentication states

--- lib/middleware/auth_bloc/auth_bloc.dart
+++ lib/middleware/auth_bloc/auth_bloc.dart
@@ -1,5 +1,9 @@
+// ignore_for_file: depend_on_referenced_packages
+
 import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
+
+import 'package:vulcane/models/user_model.dart';
 
 part 'auth_event.dart';
 part 'auth_state.dart';
--- lib/middleware/auth_bloc/auth_state.dart
+++ lib/middleware/auth_bloc/auth_state.dart
@@ -4,3 +4,17 @@ part of 'auth_bloc.dart';
 sealed class AuthState {}
 
 final class AuthInitial extends AuthState {}
+
+final class AuthLoading extends AuthState {}
+
+final class AuthSuccess extends AuthState {
+  final VulcaneUser user;
+
+  AuthSuccess({required this.user});
+}
+
+final class AuthFailure extends AuthState {
+  final String message;
+
+  AuthFailure({required this.message});
+}
