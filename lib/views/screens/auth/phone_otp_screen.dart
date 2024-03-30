0083b899bf6bd0e7f37af610488c4c832facc56e
LbnMahs <labanmahs2002@gmail.com>
2024-03-30 18:17:26 +0300

    chore: otp verification UI

--- lib/views/screens/auth/phone_otp_screen.dart
+++ lib/views/screens/auth/phone_otp_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_otp_text_field/flutter_otp_text_field.dart';
+import 'package:intl_phone_field/intl_phone_field.dart';
 
 class PhoneOTPScreen extends StatefulWidget {
   const PhoneOTPScreen({super.key});
@@ -8,11 +10,110 @@ class PhoneOTPScreen extends StatefulWidget {
 }
 
 class _PhoneOTPScreenState extends State<PhoneOTPScreen> {
+  var _isPhoneNumber = false;
+  final _messageController = TextEditingController();
+
+  @override
+  void dispose() {
+    _messageController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.primaryContainer,
       body: Center(
-        child: Text('Phone OTP Screen'),
+        child: SingleChildScrollView(
+          child: Card(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10),
+            ),
+            color: Theme.of(context).colorScheme.primary,
+            child: Padding(
+              padding: const EdgeInsets.all(15),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Text(
+                    _isPhoneNumber ?'Enter your phone number' : 'Enter the sent OTP code',
+                    style: Theme.of(context).textTheme.titleSmall!.copyWith(
+                      color: Theme.of(context).colorScheme.onPrimary,
+                    ),
+                    textAlign: TextAlign.start,
+                  ),
+                  const SizedBox(height: 10),
+                  Text(
+                    _isPhoneNumber 
+                      ? 'We will send you a verification code' 
+                      : 'Enter the code sent to your phone',
+                    style: Theme.of(context).textTheme.bodySmall!.copyWith(
+                      color: Theme.of(context).colorScheme.onPrimaryContainer,
+                    ),
+                    textAlign: TextAlign.start,
+                  ),
+                  const SizedBox(height: 20),
+                  _isPhoneNumber
+                    ? IntlPhoneField(
+                        controller: _messageController,
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10.0),
+                            borderSide: BorderSide.none,
+                          ),
+                          filled: true,
+                          fillColor: Theme.of(context).colorScheme.primaryContainer,
+                          contentPadding: const EdgeInsets.all(10.0),
+                          enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10.0),
+                            borderSide: BorderSide.none,
+                          ),
+                          errorBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10.0),
+                            borderSide: const BorderSide(color: Colors.red, width: 1.0),
+                          ),
+                          focusedErrorBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10.0),
+                            borderSide: BorderSide.none,
+                          ),
+                        ),
+                        initialCountryCode: 'KE',
+                        onChanged: (phone) => debugPrint(phone.completeNumber),
+                      )
+                    : OtpTextField(
+                        numberOfFields: 6,
+                        borderColor: Theme.of(context).colorScheme.primaryContainer,
+                        focusedBorderColor: Theme.of(context).colorScheme.onPrimary,
+                        showFieldAsBox: true,
+                        onCodeChanged: (code) {
+                          debugPrint(code);
+                        },
+                      ),
+                  const SizedBox(height: 30),
+                  ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 15, vertical: 15
+                      ),
+                      backgroundColor: Theme.of(context).colorScheme.primaryContainer,
+                      foregroundColor: Theme.of(context).colorScheme.onPrimary,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                    child: Text(
+                      _isPhoneNumber ? 'Send OTP' : 'Verify OTP',
+                      style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                        color: Theme.of(context).colorScheme.onPrimary,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
       ),
     );
   }
